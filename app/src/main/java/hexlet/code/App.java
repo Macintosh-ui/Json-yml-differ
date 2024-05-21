c76ceb0fe5ce66385908a2bed9713f7b96db50fe
Pavlov Denis <pda33@icloud.com>
2024-05-21 21:16:14 +0300

    picoc added

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,6 +1,18 @@
 package hexlet.code;
 
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
+        description = "Compares two configuration files and shows a difference.")
+
 public class App {
+
+    @Option(names = { "-h", "--help" }, usageHelp = true, description = "Show this help message and exit.")
+    private boolean helpRequested = false;
+    @Option(names = { "-v", "--version" }, description = "Print version information and exit.")
+    private String version = "gendiff 1.0";
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
