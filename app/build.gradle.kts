4e121a0e25abd78eb50b43922f82e3e22c93ccac
Pavlov Denis <pda33@icloud.com>
2024-05-30 15:10:38 +0300

    another gradle files

---
+++ app/build.gradle.kts
@@ -0,0 +1,46 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
+plugins {
+    java
+    application
+    jacoco
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("info.picocli:picocli:4.7.6")
+    implementation ("com.fasterxml.jackson.core:jackson-databind:2.14.0-rc1")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.14.2")
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {xml.required.set(true)}// tests are required to run before generating the report
+}
+tasks.test {
+    finalizedBy(tasks.jacocoTestReport)
+    useJUnitPlatform()
+    maxHeapSize = "1G"
+
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+        showStackTraces = true
+        showCauses = true
+        showStandardStreams = true
+
+    }
+}
+
+application {
+    mainClass = "hexlet.code.App"
+}
