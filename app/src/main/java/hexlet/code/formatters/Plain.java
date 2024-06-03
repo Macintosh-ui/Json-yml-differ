df12a749028f43cc5d3c156b8a4387e2ed12bcca
Pavlov Denis <pda33@icloud.com>
2024-06-03 22:11:44 +0300

    fixed plain formatter pt4. Now plain formatter gets data in Object type

--- app/src/main/java/hexlet/code/formatters/Plain.java
+++ app/src/main/java/hexlet/code/formatters/Plain.java
@@ -19,55 +19,25 @@ public class Plain {
         return output.toString();
     }
 
-    public static void addValue(String k, Object value1, Object value2, StringBuilder output) {
-        output.append("Property '").append(k).append("' was added with value: ");
-        complexCheck(k, value2, output);
-    }
-
-    public static void changeValue(String k, Object value1, Object value2, StringBuilder output) {
+    private static void changeValue(String k, Object v1, Object v2, StringBuilder output) {
         output.append("Property '").append(k).append("' was updated. From ");
-        complexCheck(k,value1, value2, output);
+        bracketCheck(v1, output);
+        output.append(" to ");
+        bracketCheck(v2, output);
+        output.append("\n");
     }
-
-    public static void complexCheck(String k, Object value2, StringBuilder output) {
-        if (String.valueOf(value2).startsWith("[") || String.valueOf(value2).startsWith("{")) {
-            output.append("[complex value]").append("\n");
-        } else {
-            output.append("'").append(value2).append("'").append("\n");
-        }
+    private static void addValue(String k, Object v1, Object v2, StringBuilder output) {
+        output.append("Property '").append(k).append("' was added with value: ");
+        bracketCheck(v2, output);
+        output.append("\n");
     }
-    public static void complexCheck(String k, String value1, String value2, StringBuilder output) {
-        if ((value1.startsWith("[") || value1.startsWith("{")) && (value2.startsWith("[") || value2.startsWith("{"))) {
-            output.append("[complex value]").append(" to ").append("[complex value]").append("\n");
-        } else if ((value1.startsWith("[") || value1.startsWith("{"))
-                && (!value2.startsWith("[") || !value2.startsWith("{"))) {
-            output.append("[complex value]").append(" to '").append(value2).append("'").append("\n");
-        } else if ((!value1.startsWith("[") || !value1.startsWith("{"))
-                && (value2.startsWith("[") || value2.startsWith("{"))) {
-            output.append("'").append(value1).append("' to ").append("[complex value]").append("\n");
+    private static void bracketCheck(Object value, StringBuilder output) {
+        if (value instanceof String) {
+            output.append("'").append(value).append("'");
+        } else if (String.valueOf(value).startsWith("[") || String.valueOf(value).startsWith("{")) {
+            output.append("[complex value]");
         } else {
-            output.append("'").append(value1).append("'").append(" to '").append(value2).append("'").append("\n");
+            output.append(value);
         }
-        /*if (value1.startsWith("[") || value1.startsWith("{")) {
-            value1 = "[complex value]";
-        }
-        if (value2.startsWith("[") || value2.startsWith("{")) {
-            value2 = "[complex value]";
-        }
-        if (value1.equals("[complex value]") && value2.equals("[complex value]")) {
-            output.append(value1).append(" to ").append(value2).append("\n");
-        } else if (value1.equals("[complex value]") && !value2.equals("[complex value]")) {
-            output.append(value1).append(" to '").append(value1).append("'").append("\n");
-        } else if (!value1.equals("[complex value]") && value2.equals("[complex value]")) {
-            output.append("'").append(value1).append("'").append(" to ").append(value2).append("\n");
-        }  else if (value1.equals("true") || value1.equals("false") ||
-        value2.equals("true") || value2.equals("false")) {
-            output.append(value1).append(" to ").append(value2).append("\n");
-        }
-        else {
-            output.append("'").append(value1).append("'").append(" to '").append(value2).append("'").append("\n");
-        }*/
-
-
     }
 }
