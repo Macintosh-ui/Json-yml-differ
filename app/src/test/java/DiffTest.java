dc5f554f15471816600c5dd10a792545dcd31151
Pavlov Denis <pda33@icloud.com>
2024-06-04 10:05:24 +0300

    test checkstyle fix

--- app/src/test/java/DiffTest.java
+++ app/src/test/java/DiffTest.java
@@ -10,7 +10,8 @@ public class DiffTest {
 
     @Test
     public void testFilepath() {
-        String expected = "/home/runner/work/java-project-71/java-project-71/app/src/test/resources/fixtures/file1.json";
+        String expected =
+                "/home/runner/work/java-project-71/java-project-71/app/src/test/resources/fixtures/file1.json";
         String actual = String.valueOf(Differ.getFilePath("src/test/resources/fixtures/file1.json"));
         assertEquals(expected, actual);
     }
