b571ab94bc647bc72f9e2bc2554505998e36b715
Pavlov Denis <pda33@icloud.com>
2024-06-04 10:03:12 +0300

    test updated to github

--- app/src/test/java/DiffTest.java
+++ app/src/test/java/DiffTest.java
@@ -10,7 +10,7 @@ public class DiffTest {
 
     @Test
     public void testFilepath() {
-        String expected = "/Users/denis/java-project-71/app/src/test/resources/fixtures/file1.json";
+        String expected = "/home/runner/work/java-project-71/java-project-71/app/src/test/resources/fixtures/file1.json";
         String actual = String.valueOf(Differ.getFilePath("src/test/resources/fixtures/file1.json"));
         assertEquals(expected, actual);
     }
