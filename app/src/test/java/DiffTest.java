b82fc651c1914887264b01cb8b65b41cee960cfc
Pavlov Denis <pda33@icloud.com>
2024-06-04 11:14:24 +0300

    added plain YAML test

--- app/src/test/java/DiffTest.java
+++ app/src/test/java/DiffTest.java
@@ -33,8 +33,16 @@ public class DiffTest {
     }
     @Test
     public void testPlainDiff() throws Exception {
-        var expected = Files.readString(Paths.get("src/test/resources/fixtures/expectedStylish")).trim();
+        var expected = Files.readString(Paths.get("src/test/resources/fixtures/expectedPlain")).trim();
         String actual = Differ.generate("src/test/resources/fixtures/file3.JSON",
                                         "src/test/resources/fixtures/file4.JSON", "plain");
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void testplainymlDiff() throws Exception {
+        var expected = Files.readString(Paths.get("src/test/resources/fixtures/expectedPlain")).trim();
+        String actual = Differ.generate("src/test/resources/fixtures/PlaintTest1.yml",
+                                        "src/test/resources/fixtures/PlaintTest2.yml", "plain");
+        assertEquals(expected, actual);
     }
 }
