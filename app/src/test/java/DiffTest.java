ba94ecb51f73ed33bf38a7c4abfb2a5e937f2c15
Pavlov Denis <pda33@icloud.com>
2024-06-20 14:28:18 +0300

    Test refactoring and checkstyle fixes

--- app/src/test/java/DiffTest.java
+++ app/src/test/java/DiffTest.java
@@ -13,12 +13,13 @@ public class DiffTest {
     private static String expectedStylish;
     private static String expectedPlain;
     private static String expectedJson;
+    private static String filepathPart = "src/test/resources/fixtures/";
 
     @BeforeAll
     public static void setUp() throws IOException {
-        expectedStylish = readFile("src/test/resources/fixtures/expectedStylish");
-        expectedPlain = readFile("src/test/resources/fixtures/expectedPlain");
-        expectedJson = String.valueOf(Files.readAllLines(Paths.get("src/test/resources/fixtures/expected.json")));
+        expectedStylish = readFile(filepathPart + "expectedStylish");
+        expectedPlain = readFile(filepathPart + "expectedPlain");
+        expectedJson = String.valueOf(Files.readAllLines(Paths.get(filepathPart + "expected.json")));
     }
 
     private static String readFile(String filepath) throws IOException {
@@ -27,59 +28,51 @@ public class DiffTest {
 
     @Test
     public void testYamlStylishDiff() throws Exception {
-        String actual = Differ.generate("src/test/resources/fixtures/file1.yml",
-                                        "src/test/resources/fixtures/file2.yml", "stylish");
+        String actual = Differ.generate(filepathPart + "file1.yml", filepathPart + "file2.yml", "stylish");
         assertEquals(expectedStylish, actual);
     }
 
     @Test
     public void testJsonStylishDiff() throws Exception {
-        String actual = Differ.generate("src/test/resources/fixtures/file3.json",
-                                        "src/test/resources/fixtures/file4.json", "stylish");
+        String actual = Differ.generate(filepathPart + "file3.json", filepathPart + "file4.json", "stylish");
         assertEquals(expectedStylish, actual);
     }
 
     @Test
     public void testJsonPlainDiff() throws Exception {
-        String actual = Differ.generate("src/test/resources/fixtures/file3.json",
-                                        "src/test/resources/fixtures/file4.json", "plain");
+        String actual = Differ.generate(filepathPart + "file3.json", filepathPart + "file4.json", "plain");
         assertEquals(expectedPlain, actual);
     }
 
     @Test
     public void testYmlPlainDiff() throws Exception {
-        String actual = Differ.generate("src/test/resources/fixtures/file1.yml",
-                                        "src/test/resources/fixtures/file2.yml", "plain");
+        String actual = Differ.generate(filepathPart + "file1.yml", filepathPart + "file2.yml", "plain");
         assertEquals(expectedPlain, actual);
     }
 
     @Test
     public void testJsonToJsonFormat() throws Exception {
         ObjectMapper mapper = new ObjectMapper();
-        String actual = Differ.generate("src/test/resources/fixtures/file3.json",
-                                        "src/test/resources/fixtures/file4.json", "json");
+        String actual = Differ.generate(filepathPart + "file3.json", filepathPart + "file4.json", "json");
         assertEquals(mapper.readTree(expectedJson), mapper.readTree(actual));
     }
 
     @Test
     public void testYamlToJsonFormat() throws Exception {
         ObjectMapper mapper = new ObjectMapper();
-        String actual = Differ.generate("src/test/resources/fixtures/file1.yml",
-                                        "src/test/resources/fixtures/file2.yml", "json");
+        String actual = Differ.generate(filepathPart + "file1.yml", filepathPart + "file2.yml", "json");
         assertEquals(mapper.readTree(expectedJson), mapper.readTree(actual));
     }
 
     @Test
     public void testJsonNoArgs() throws Exception {
-        String actual = Differ.generate("src/test/resources/fixtures/file3.json",
-                        "src/test/resources/fixtures/file4.json");
+        String actual = Differ.generate(filepathPart + "file3.json", filepathPart + "file4.json");
         assertEquals(expectedStylish, actual);
     }
 
     @Test
     public void testYamlNoArgs() throws Exception {
-        String actual = Differ.generate("src/test/resources/fixtures/file1.yml",
-                                        "src/test/resources/fixtures/file2.yml");
+        String actual = Differ.generate(filepathPart + "file1.yml", filepathPart + "file2.yml");
         assertEquals(expectedStylish, actual);
     }
 }
